09ff296192ab13b0c8fd4f532e96308e9e894269
Thomas de Groot <degroot12@gmail.com>
2021-01-28 15:33:42 +0100

    added underworld game

--- index.js
+++ index.js
@@ -7,66 +7,82 @@ let penaltyPoints = 0;
 let hamiltonScore = 250;
 let lives = 10
 
+let zombieCarsKilled = 0
+
 // AUDIO
 
 
 let superMaxSound = new Audio()
 superMaxSound.src = 'sounds/superMaxLowSound.mp3'
-superMaxSound.volume = 0.5
+superMaxSound.volume = 0
 
 let lewisWinSound = new Audio()
 lewisWinSound.src = 'sounds/losingSound.mp3'
-lewisWinSound.volume = 0.5
+lewisWinSound.volume = 0
 
 let victoryMusic = new Audio()
 victoryMusic.src = 'sounds/victoryMusic.mp3'
-victoryMusic.volume = 0.5
+victoryMusic.volume = 0
+
+
 
 
 // BEGIN AND ENDSCREENS
 
-let startGameBtn = document.querySelector('.startGameBtn')
-let splash = document.querySelector('.splash')
+const startGameBtn = document.querySelector('.startGameBtn')
+const splash = document.querySelector('.splash')
 
-let playAgainBtn = document.querySelector('.playAgainBtn')
+const playAgainBtn = document.querySelector('.playAgainBtn')
 
-let playAgainBtnWin = document.querySelector('.playAgainBtnWin')
+const playAgainBtnWin = document.querySelector('.playAgainBtnWin')
 
-let lewisWins = document.querySelector('.losing-screen')
+const lewisWins = document.querySelector('.losing-screen')
 
-let maxWins = document.querySelector('.victory-screen')
+const maxWins = document.querySelector('.victory-screen')
 
-let easyBtn = document.querySelector('.easyBtn')
+const easyBtn = document.querySelector('.easyBtn')
 
-let mediumBtn = document.querySelector('.mediumBtn')
+const mediumBtn = document.querySelector('.mediumBtn')
 
-let hardBtn = document.querySelector('.hardBtn')
+const hardBtn = document.querySelector('.hardBtn')
+
+const muteBtn = document.querySelector('.muteBtn')
 
 // IMAGES USED IN GAME
 
-let bgImg = document.createElement('img');
+const bgImg = document.createElement('img');
 bgImg.src = 'images/backgroundColorGrass.png'
 
-let curbs = document.createElement('img')
+const curbs = document.createElement('img')
 curbs.src = 'images/curbs.png'
 
-let maxImg = document.createElement('img');
+const maxImg = document.createElement('img');
 maxImg.src = 'images/MaxCarScaled.png'
 
 
-let carImg = document.createElement('img')
+const carImg = document.createElement('img')
 carImg.src = 'images/CharlesCar.png'
 
-let pointsImg = document.createElement('img')
+const pointsImg = document.createElement('img')
 pointsImg.src = 'images/platformPack_item002.png'
 
-let blockImg = document.createElement('img')
+const boostImg = document.createElement('img')
+boostImg.src = 'images/platformPack_item018.png'
+
+const bulletImg = document.createElement('img')
+bulletImg.src = 'images/bullet.png'
+
+const zombieImg = document.createElement('img')
+zombieImg.src = 'images/ZombieCar.png'
+
+
+const blockImg = document.createElement('img')
 blockImg.src = 'images/block-to-jump.png'
 
-let longBlock = document.createElement('img')
+const longBlock = document.createElement('img')
 longBlock.src = 'images/betterLong.png'
 
-let planeImg = document.createElement('img')
+const planeImg = document.createElement('img')
 planeImg.src = 'images/TeamInstinctPlane.png'
 
 
@@ -78,12 +94,17 @@ let points = [{x: canvas.width + 50, y: 670}]
 
 let oilPatch = [{x: canvas.width + 100, y: 720}]
 
-let blocks = [{x: canvas.width + 50, y: 580}]
-
+let boosts = [{x: canvas.width + 10, y: 670}]
 
 let longBlocks = [{x: canvas.width + 50, y: 450}]
 
 let topPoints = [{x: canvas.width + 200, y: 400}]
+
+let zombieCars = [{x: canvas.width + 200, y: 690}]
+
+
+let bulletX = 190
+let bullets = [{x: 190, y: 750}]
 
 
 let planes = [{x: canvas.width + 300, y: 50}]
@@ -92,10 +113,14 @@ let blockY = 480
 
 let jumping = false
 
+let normalGame = true
+
 
 const maxX = 100;
 let maxY = 650;
-maxIncrement = 0
+let maxIncrement = 0
+
+
 
 
 
@@ -113,6 +138,13 @@ mediumBtn.addEventListener('click', () => {
 hardBtn.addEventListener('click', () => {
     hamiltonScore = 400
     lives = 3
+})
+
+muteBtn.addEventListener('click', () => {
+    superMaxSound.volume = 0.5
+    lewisWinSound.volume = 0.5
+    victoryMusic.volume = 0.5
+
 })
 
 startGameBtn.addEventListener('click', () => {
@@ -136,17 +168,24 @@ playAgainBtnWin.addEventListener('click', () => {
 // JUMP LOGIC
 
 document.addEventListener('keydown', (event) => {
-    if(event.keyCode == 32 && jumping === false){
+    if(event.keyCode == 32 && jumping === false &&(normalGame)){
         maxIncrement = -5
         jumping = true
     }
+    // if(event.keyCode == 32 &&(!normalGame)){
+    //     bullets.push({x: bulletX, y: 750})
+    // }
     
 })
 
 document.addEventListener('keyup', () => {
-    maxIncrement = 2
+    if(normalGame){
+        maxIncrement = 2
+    }
+    
 
 })
+
 
 
 // DRAW
@@ -177,14 +216,14 @@ function draw(){
         ctx.drawImage(carImg, cars[i].x, cars[i].y)
         cars[i].x = cars[i].x -2
 
-        if(cars[i].x == 194 ){
+        if(cars[i].x == 194 &&(normalGame)){
             cars.push({
                 x:canvas.width + 10,
                 y: 650
             })
         }
 
-        if(maxX+maxImg.width >= cars[i].x +30 && maxX  <= cars[i].x + carImg.width && maxY >= cars[i].y ){
+        if(maxX+maxImg.width >= cars[i].x +30 && maxX  <= cars[i].x + carImg.width && maxY >= cars[i].y && (normalGame)){
             score = score -12
             penaltyPoints++
             cars.splice(i, 1)
@@ -213,13 +252,13 @@ function draw(){
         ctx.closePath()
         oilPatch[i].x = oilPatch[i].x -2
 
-        if(oilPatch[i].x == 500){
+        if(oilPatch[i].x == 500 && (normalGame)){
             oilPatch.push({
                 x:canvas.width + 60,
                 y: 720
             })
         }
-        if(maxX+maxImg.width== oilPatch[i].x +30 && maxY >= oilPatch[i].y - 71){
+        if(maxX+maxImg.width== oilPatch[i].x +30 && (maxY >= oilPatch[i].y - 71 && (normalGame))){
             score = score -12
             penaltyPoints++
             maxImg.src = 'images/MaxCarReverse.png'
@@ -249,13 +288,13 @@ function draw(){
         ctx.drawImage(pointsImg, points[i].x, points[i].y)
         points[i].x--
 
-        if(points[i].x == 40){
+        if(points[i].x == 40 &&(normalGame)){
             points.push({
                 x:canvas.width + 60,
                 y: 670
             })
         }
-        if(maxX+maxImg.width >= points[i].x +30 && maxX  <= points[i].x + pointsImg.width  && maxY >= points[i].y -90 + pointsImg.height){
+        if(maxX+maxImg.width >= points[i].x +30 && maxX  <= points[i].x + pointsImg.width  && maxY >= points[i].y -90 + pointsImg.height && (normalGame)){
             score = score +25
             points.splice(i, 1)
             points.push({
@@ -274,18 +313,19 @@ function draw(){
         }
      
     }
+    
 
     for(let i = 0; i <topPoints.length; i++){
         ctx.drawImage(pointsImg, topPoints[i].x, topPoints[i].y)
         topPoints[i].x--
 
-        if(topPoints[i].x == 10){
+        if(topPoints[i].x == 10 &&(normalGame)){
             topPoints.push({
                 x:canvas.width + 60,
                 y: 400
             })
         }
-        if(maxX+maxImg.width >= topPoints[i].x +30 && maxX  <= topPoints[i].x + pointsImg.width  && maxY <= topPoints[i].y  + pointsImg.height){
+        if(maxX+maxImg.width >= topPoints[i].x +30 && maxX  <= topPoints[i].x + pointsImg.width  && maxY <= topPoints[i].y  + pointsImg.height &&(normalGame)){
             score = score +25
             topPoints.splice(i, 1)
             topPoints.push({
@@ -304,6 +344,121 @@ function draw(){
         }
      
     }
+
+    
+
+    // BONUS GAME
+
+    for(let i = 0; i <boosts.length; i++){
+        ctx.drawImage(boostImg, boosts[i].x, boosts[i].y)
+        boosts[i].x--
+
+        if(boosts[i].x == 40 && (normalGame)){
+            boosts.push({
+                x:canvas.width + 60,
+                y: 670
+            })
+        }
+        if(maxX+maxImg.width >= boosts[i].x +30 && maxX  <= boosts[i].x + boostImg.width  && maxY >= boosts[i].y -90 + boostImg.height && (normalGame)){
+            boosts.splice(i, 1)
+            boosts.push({
+                x:canvas.width + 60,
+                y: 670
+            })
+            maxImg.src = 'images/MaxCarUpsideDown.png'
+            maxY = 690
+            normalGame = false
+            bgImg.src = 'images/backgroundDarkWhite.png'
+        }
+     
+    }
+    if(!normalGame){
+        for(let i = 0; i <zombieCars.length; i++){
+            ctx.drawImage(zombieImg, zombieCars[i].x, zombieCars[i].y)
+            zombieCars[i].x = zombieCars[i].x -2
+    
+            if(zombieCars[i].x == 194 ){
+                zombieCars.push({
+                    x:canvas.width + 10,
+                    y: 690
+                })
+            }
+
+        for(let j = 0; j <bullets.length; j++){
+            ctx.drawImage(bulletImg, bullets[j].x, bullets[j].y)
+            bullets[j].x++
+
+            ctx.font ='50px Verdana'
+            ctx.font.style = 'yellow'
+            ctx.fillText('UNDERGROUNDS CARS KILLED: ' + zombieCarsKilled + `/10`, 10, 30)
+
+
+            if(bullets[j].x >= zombieCars[i].x +30){
+                zombieCars.splice(i, 1)
+                zombieCars.push({
+                    x:canvas.width + 60,
+                    y: 690
+                })
+                bullets.splice(j, 1)
+                
+                zombieCarsKilled++
+                if(zombieCarsKilled > 10){
+                    maxImg.src = 'images/MaxCarScaled.png'
+                    maxY = 650
+                    normalGame = true
+                    bgImg.src = 'images/backgroundColorGrass.png'
+                    cars.push({
+                        x:canvas.width + 10,
+                        y: 650
+                    })
+                    oilPatch.push({
+                        x:canvas.width + 60,
+                        y: 720
+                    })
+                    points.push({
+                        x:canvas.width + 60,
+                        y: 670
+                    })
+                    topPoints.push({
+                        x:canvas.width + 60,
+                        y: 400
+                    })
+
+                }
+            }
+
+
+        }
+
+            document.addEventListener('keydown', (event) => {
+                if(event.keyCode == 32 &&(!normalGame) && bullets.length <= 1){
+                    bullets.push({
+                        x: 190,
+                        y: 750
+                    })
+                }
+                
+            })
+          
+    
+            if(maxX+maxImg.width >= zombieCars[i].x +30 && maxX  <= zombieCars[i].x + zombieImg.width && maxY >= zombieCars[i].y){
+                score = score -12
+                penaltyPoints++
+                zombieCars.splice(i, 1)
+         
+                if(penaltyPoints >= lives){
+                    superMaxSound.src = ""
+                    lewisWinSound.play()
+                    clearInterval(intervalID)
+                    lewisWins.style.display = 'flex'
+                }
+            }
+    
+        }
+    }
+    
+
+
 
 
 
